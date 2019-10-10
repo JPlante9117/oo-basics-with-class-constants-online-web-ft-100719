9069149b03bb163f33a405b6a8ce1f22ee8c174a
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-10 17:54:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    unless BRANDS.include?(brand)
+      BRANDS << self.brand
+    end
   end
 
   def cobble
@@ -15,9 +18,7 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    unless BRANDS.include?(brand)
-      BRANDS << self.brand
-    end
+
   end
 
 end
